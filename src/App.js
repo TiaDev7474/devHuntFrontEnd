714db1cb6f90f064ac798d68e71f9551c7428bc6
TiaDev7474 <nomenjanaharyriry@gmail.com>
2023-03-11 11:29:28 +0100

    config routes global finish

--- src/App.js
+++ src/App.js
@@ -1,9 +1,19 @@
-import Form from './compenents/Forms';
+
+import { Route, Routes } from 'react-router-dom';
 import './App.css';
+import Authpage from './Pages/Authentification';
+import Dashboard from './Pages/Dashboard';
+import RootPage from './Pages/Rootpage';
 
 function App() {
   return (
-     <div></div>
+     <div>
+          <Routes>
+               <Route path='/admin/*' element={<Dashboard/>}/>
+               <Route path='/user/*' element={<RootPage/>}/>
+               <Route path='/auth/*' element={<Authpage/>}/>
+          </Routes>
+     </div>
   )
 }
 
