22b853efdde137c9977e3f1ab475bb7cd285c25e
andrianiainafn <fanomezantsoanomenandrianiaina@gmail.com>
2023-03-13 22:12:31 +0300

    mise en place du front de l' auth

--- src/Pages/forum/LayoutForum.jsx
+++ src/Pages/forum/LayoutForum.jsx
@@ -1,16 +1,22 @@
 import React from 'react'
 import { Outlet } from 'react-router-dom'
-import { Sidebar} from '../../compenents'
-import { ForumLeftBar } from '../../features/forum'
+import { Sidebar,Feed,Rightbar } from '../../compenents'
+import { ForumLeftBar } from '../../features/forum/components'
+import ForumRightBar from '../../features/forum/components/ui/ForumRightBar'
 
 const ForumLayout = () => {
   return (
-    <>
+    <div className="relative w-full">
       <Sidebar>
-        <ForumLeftBar/>
+          <ForumLeftBar/>
       </Sidebar>
-      <Outlet/>
-    </>
+      <Feed>
+        <Outlet/>
+      </Feed>
+      <Rightbar>
+         <ForumRightBar/>
+      </Rightbar>
+    </div>
   )
 }
 
