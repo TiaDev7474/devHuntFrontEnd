81ef07d4d53d36fdc8d6241d10af0ccb3e5587d7
TiaDev7474 <nomenjanaharyriry@gmail.com>
2023-03-13 12:33:24 +0100

    responsive issue fixed

--- src/features/publication/components/card/card.jsx
+++ src/features/publication/components/card/card.jsx
@@ -1,10 +1,11 @@
 import React from 'react'
+import UserIcon from '../../../../assets/imagess/user.svg'
 
 function Card() {
   return (
-    <div className='w-full h-[400px] bg-black'>
-          <div className='flex'>
-
+    <div className='w-full h-[400px] py-3 px-10 flex flex-col items-start bg-black'>
+          <div className='flex '>
+               <img src={UserIcon}  alt='user profile' className='w-14 h-14'/>
           </div>
 
     </div>
